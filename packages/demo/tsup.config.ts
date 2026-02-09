24f2f24788d28e8ce6d71cd652717defa868fbdd
William Correa <wilcorrea@gmail.com>
2026-02-09 00:37:16 -0300

    refactor(demo): remove i18n and faker from demo package

--- packages/demo/tsup.config.ts
+++ packages/demo/tsup.config.ts
@@ -1,10 +1,10 @@
 import { defineConfig } from 'tsup'
 
 export default defineConfig({
-  entry: ['src/index.ts', 'src/settings/i18n.ts'],
+  entry: ['src/index.ts'],
   format: ['esm'],
   dts: true,
   clean: true,
   sourcemap: true,
-  external: ['@anhanga/core', '@faker-js/faker', 'react', 'i18next', 'react-i18next'],
+  external: ['@anhanga/core', '@faker-js/faker'],
 })
