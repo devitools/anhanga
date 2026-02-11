9c54f6313995026528c8642677402c2a0293c697
William Correa <wilcorrea@gmail.com>
2026-02-11 13:18:06 -0300

    fix(playground): use web driver on web platform

--- playground/react-native/src/demo.ts
+++ playground/react-native/src/demo.ts
@@ -1,7 +1,9 @@
+import { Platform } from 'react-native'
 import { createLocalDriver } from '@anhanga/persistence'
+import { createWebDriver } from '@anhanga/persistence/web'
 import { createPersonService, createPersonHandlers, createPersonHooks } from '@anhanga/demo'
 
-const driver = createLocalDriver()
+const driver = Platform.OS === 'web' ? createWebDriver() : createLocalDriver()
 export const personService = createPersonService(driver)
 export const personHandlers = createPersonHandlers(personService)
 export const personHooks = createPersonHooks(personService)
